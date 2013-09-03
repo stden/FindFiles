51660192da809ac9f226bb1a2fdeea43a6ab9448
Denis Stepulenok <super.denis@gmail.com>
2013-09-03 20:11:53 +0400

    Исправлен скрипт создания БД

--- src/mysql.sql
+++ src/mysql.sql
@@ -1,11 +1,9 @@
-CREATE TABLE FindFiles.results (
+CREATE DATABASE FindFiles;
+
+CREATE TABLE FindFiles.FindResult (
   id      INT          NOT NULL AUTO_INCREMENT,
-  keyword VARCHAR(255) NOT NULL
-  COMMENT 'Ключевое слово',
-  path    VARCHAR(255) NOT NULL
-  COMMENT 'Имя файла',
-  line    INT          NOT NULL
-  COMMENT 'Строка',
+  keyword VARCHAR(255) NOT NULL COMMENT 'Ключевое слово',
+  path    VARCHAR(255) NOT NULL COMMENT 'Имя файла',
+  line    INT          NOT NULL COMMENT 'Строка',
   PRIMARY KEY (id)
-)
-  ENGINE = INNODB;
+) ENGINE = INNODB;
